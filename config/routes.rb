331ba9ed5aa0faca7aaa15e1615205431fe2359f
Ethan Li <ethanli588@gmail.com>
2022-05-30 16:24:56 +1000

    routes completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pets, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :edit, :update]
 end
