a6807e3f532252682e3365f972716dd04b879263
Xander Wellingham <xwellingham@gmail.com>
2022-06-01 17:01:36 +1000

    seeds correct

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,45 +33,45 @@ pet_photo = ['https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/dome
 
 i = 1
 12.times do
-  pet = Pet.new(name: DOG_NAMES.sample, species: 'dog', toilet_trained: TOILET_TRAINED.sample, description: "best pet ever, man and womans best friend, sometimes pees on the floor but still very lovable", price_per_day: rand(25..100), user_id: rand(1..4))
+  pet = Pet.new(name: DOG_NAMES.sample, species: 'dog', toilet_trained: TOILET_TRAINED.sample, description: "best pet ever, man and womans best friend, sometimes pees on the floor but still very lovable", price_per_day: rand(25..100), user: User.all.sample)
   file = URI.open(pet_photo.sample)
   pet.photos.attach(io: file, filename: "dog#{i}.png", content_type: 'image/png')
-  pet.save
+  pet.save!
   i += 1
 end
 
 p 'random seeds completed'
 
-rabbit = Pet.new(name: DOG_NAMES.sample, species: 'dog', toilet_trained: TOILET_TRAINED.sample, description: "hops a lot, big ball of fluff - great for hugs", price_per_day: rand(25..100), user_id: rand(1..4))
+rabbit = Pet.new(name: DOG_NAMES.sample, species: 'dog', toilet_trained: TOILET_TRAINED.sample, description: "hops a lot, big ball of fluff - great for hugs", price_per_day: rand(25..100), user: User.all.sample)
 file = URI.open('https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80')
 rabbit.photos.attach(io: file, filename: "rabbittwo.png", content_type: 'image/png')
 file = URI.open('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQimKYQiAFfFqC-2xlcGaf9WEaEBKxVoCp-4Q&usqp=CAU')
 rabbit.photos.attach(io: file, filename: "rabbitone.png", content_type: 'image/png')
 file = URI.open('https://kowloon-vet-hospital.com/wp-content/uploads/2017/11/Rabbit.jpg')
 rabbit.photos.attach(io: file, filename: "rabbitthree.png", content_type: 'image/png')
-rabbit.save
+rabbit.save!
 
 p 'rabbit seeded'
 
-wallaby = Pet.new(name: DOG_NAMES.sample, species: 'wallaby', toilet_trained: TOILET_TRAINED.sample, description: "bit wild but much nicer than a kangaroo", price_per_day: rand(25..100), user_id: rand(1..4))
+wallaby = Pet.new(name: DOG_NAMES.sample, species: 'wallaby', toilet_trained: TOILET_TRAINED.sample, description: "bit wild but much nicer than a kangaroo", price_per_day: rand(25..100), user: User.all.sample)
 file = URI.open('https://animals.sandiegozoo.org/sites/default/files/inline-images/western-grey-kangaroo.jpg')
 wallaby.photos.attach(io: file, filename: "wallabyone.png", content_type: 'image/png')
 file = URI.open('https://upload.wikimedia.org/wikipedia/commons/7/74/Red_necked_wallaby444.jpg')
 wallaby.photos.attach(io: file, filename: "wallabytwo.png", content_type: 'image/png')
 file = URI.open('https://w2.chabad.org/media/images/1122/FuEe11225216.jpg?_i=_n504BC99DD0473598AAE3BCDC5D75568D')
 wallaby.photos.attach(io: file, filename: "wallabythree.png", content_type: 'image/png')
-wallaby.save
+wallaby.save!
 
 p 'wallaby seeded'
 
-dog = Pet.new(name: DOG_NAMES.sample, species: 'dog', toilet_trained: TOILET_TRAINED.sample, description: "best pet ever, man and woman's best friend. Sometimes pees on the floor but still very lovable", price_per_day: rand(25..100), user_id: rand(1..4))
+dog = Pet.new(name: DOG_NAMES.sample, species: 'dog', toilet_trained: TOILET_TRAINED.sample, description: "best pet ever, man and woman's best friend. Sometimes pees on the floor but still very lovable", price_per_day: rand(25..100), user: User.all.sample)
 file = URI.open('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg')
 dog.photos.attach(io: file, filename: "dogone.png", content_type: 'image/png')
 file = URI.open('https://www.purina.com.au/-/media/project/purina/main/breeds/puppies/puppy-golden-retriever.jpg')
 dog.photos.attach(io: file, filename: "dogtwo.png", content_type: 'image/png')
 file = URI.open('https://i.pinimg.com/originals/32/e2/b8/32e2b8e0d273c84dce7ded590d56bbfe.jpg')
 dog.photos.attach(io: file, filename: "dogthree.png", content_type: 'image/png')
-dog.save
+dog.save!
 
 p 'dog example seeded'
 p 'seed complete'
