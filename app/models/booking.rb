d61459a39815355860a6801f04b1b6cbd8ead3af
Xander Wellingham <xwellingham@gmail.com>
2022-06-01 13:26:36 +1000

    booking end date after start date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,11 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pet
   validates :start_date, presence: true
-  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+  validate :end_time_after_start_time
+
+  private
+
+  def end_time_after_start_time
+    errors.add(:end_date, "can't be in the past") unless end_date > start_date
+  end
 end
